f67d448960cc8e88f8e136e166a892d1210f8117
karenheyn <karenheyn@gmail.com>
2024-03-06 17:20:11 -0500

    new icons, added phone, new nav

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ function sendMail() {
   window.open(`mailto:qualtechar@gmail.com?subject=${subject}&body=${message}`);
   submit();
 }
+const menu_btn = document.querySelector(".menu-btn");
+const close_btn = document.querySelector(".close-btn");
+const menu = document.querySelector(".menu");
+const drawer = menu.querySelector(".drawer");
+const blank = menu.querySelector(".blank");
+const close = () => {
+  menu.classList.remove("blur");
+  drawer.classList.remove("drawer-visible");
+};
+
+menu_btn.addEventListener("click", (e) => {
+  menu.classList.add("blur");
+  drawer.classList.add("drawer-visible");
+});
+
+close_btn.addEventListener("click", (e) => {
+  close();
+});
+
+blank.addEventListener("click", (e) => {
+  close();
+});
+
+Array.from(drawer.querySelectorAll("a")).forEach((element) => {
+  element.addEventListener("click", () => {
+    close();
+  });
+});
