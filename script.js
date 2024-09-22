33e05e033cb68f155db680113e85084223dbe3f1
karenheyn <karenheyn@gmail.com>
2024-09-21 22:27:39 -0400

    new nav, SEO

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
-const details = document.querySelectorAll("details");
-details.forEach((targetDetail) => {
-  targetDetail.addEventListener("click", () => {
-    // Close all the details that are not targetDetail.
-    details.forEach((detail) => {
-      if (detail !== targetDetail) {
-        detail.removeAttribute("open");
-      }
-    });
-  });
-});
+// const details = document.querySelectorAll("details");
+// const mainNav = document.getElementsByClassName(".nav");
+// const subNav = document.getElementsByClassName(".sub-nav");
+// details.forEach((targetDetail) => {
+//   targetDetail.addEventListener("click", () => {
+//     // Close all the details that are not targetDetail.
+//     details.forEach((detail) => {
+//       if (detail !== targetDetail) {
+//         detail.removeAttribute("open");
+//       }
+//     });
+//   });
+// });
 
-const button = document.getElementById("submit");
+// const button = document.getElementById("submit");
 
-function sendMail() {
-  const message = document.getElementById("body").value;
-  const subject = document.getElementById("subject").value;
-  window.open(`mailto:qualtechar@gmail.com?subject=${subject}&body=${message}`);
-  submit();
-}
-const menu_btn = document.querySelector(".menu-btn");
-const close_btn = document.querySelector(".close-btn");
-const menu = document.querySelector(".menu");
-const drawer = menu.querySelector(".drawer");
-const blank = menu.querySelector(".blank");
-const close = () => {
-  menu.classList.remove("blur");
-  drawer.classList.remove("drawer-visible");
-};
+// function sendMail() {
+//   const message = document.getElementById("body").value;
+//   const subject = document.getElementById("subject").value;
+//   window.open(`mailto:qualtechar@gmail.com?subject=${subject}&body=${message}`);
+//   submit();
+// }
+// const menu_btn = document.querySelector(".menu-btn");
+// const close_btn = document.querySelector(".close-btn");
+// const menu = document.querySelector(".menu");
+// const drawer = menu.querySelector(".drawer");
+// const blank = menu.querySelector(".blank");
+// const close = () => {
+//   menu.classList.remove("blur");
+//   drawer.classList.remove("drawer-visible");
+// };
 
-menu_btn.addEventListener("click", (e) => {
-  menu.classList.add("blur");
-  drawer.classList.add("drawer-visible");
-});
+// menu_btn.addEventListener("click", (e) => {
+//   menu.classList.add("blur");
+//   drawer.classList.add("drawer-visible");
+// });
 
-close_btn.addEventListener("click", (e) => {
-  close();
-});
+// close_btn.addEventListener("click", (e) => {
+//   close();
+// });
 
-
-Array.from(drawer.querySelectorAll("a")).forEach((element) => {
-  element.addEventListener("click", () => {
-    close();
-  });
-});
-menu.addEventListener('click', (e) => {
-  menu.classList.remove("blur");
-  drawer.classList.remove("drawer-visible");
-});
+// Array.from(drawer.querySelectorAll("a")).forEach((element) => {
+//   element.addEventListener("click", () => {
+//     close();
+//   });
+// });
+// menu.addEventListener("click", (e) => {
+//   menu.classList.remove("blur");
+//   drawer.classList.remove("drawer-visible");
+// });
